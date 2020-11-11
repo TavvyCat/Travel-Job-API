8ca58dc92be32796270452c609283daea02f0b85
TavvyCat <tavish.cary@welch.edu>
2020-11-10 18:53:08 -0600

    Changed DB name
    Changed name of database to "travel-jobs-site".

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'pt-job-site'
+const mongooseBaseName = 'travel-jobs-api'
 
 // create the mongodb uri for development and test
 const database = {
